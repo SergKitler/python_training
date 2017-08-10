5ecbf181ca93c4033f9a73db8c87c16670e3fc26
SergKitler <svkitler@gmail.com>
2017-08-10 23:58:58 +0700

    Добавлены тесты изменения пользователя через страницу new birthdays

--- fixture/user.py
+++ fixture/user.py
@@ -79,3 +79,31 @@ class UserHelper:
         self.fill_user_form(group)
         #update user edition
         wd.find_element_by_name("update").click()
+
+    def edit_first_user_via_birthday_deteils(self,group):
+        wd = self.app.wd
+        #open birthday page
+        self.open_birthday_page()
+        #select first user
+        wd.find_element_by_xpath("//table[@id='birthdays']/tbody/tr[2]/td[7]/a/img").click()
+        #start modify user form
+        wd.find_element_by_name("modifiy").click()
+        #fill user form
+        self.fill_user_form(group)
+        #update user edition
+        wd.find_element_by_name("update").click()
+
+    def edit_first_user_via_birthday_edit(self,group):
+        wd = self.app.wd
+        # open birthday page
+        self.open_birthday_page()
+        # start modify first user form
+        wd.find_element_by_xpath("//table[@id='birthdays']/tbody/tr[2]/td[8]/a/img").click()
+        #fill user form
+        self.fill_user_form(group)
+        #update user edition
+        wd.find_element_by_name("update").click()
+
+    def open_birthday_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("next birthdays").click()
--- test/test_edit_user.py
+++ test/test_edit_user.py
@@ -14,3 +14,15 @@ def test_edit_user_via_edit(app):
     app.user.edit_first_user_via_edit(User(firstname="Daniil", middlename="Ivanovich", lastname="Sidorov", nickname="TestUser", title="test", company="TestingGroup",
                                   email="iii@testinggroup.com", byear="1982"))
     app.session.logout()
+
+def test_edit_user_via_birthday_details(app):
+    app.session.login(username="admin", password="secret")
+    app.user.edit_first_user_via_birthday_deteils(User(firstname="Mikhael", middlename="Ivanovich", lastname="Sidorov", nickname="TestUser", title="test", company="TestingGroup",
+                                  email="iii@testinggroup.com", byear="1980"))
+    app.session.logout()
+
+def test_edit_user_via_birthday_edit(app):
+    app.session.login(username="admin", password="secret")
+    app.user.edit_first_user_via_birthday_edit(User(firstname="Mikhael", middlename="Ivanovich", lastname="Sidorov", nickname="TestUser", title="test", company="TestingGroup",
+                                  email="iii@testinggroup.com", byear="1980"))
+    app.session.logout()
