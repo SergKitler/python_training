98d03b88001c79e41dde24234b54383912831832
SergKitler <svkitler@gmail.com>
2017-08-10 23:41:14 +0700

    Изменение фикстуры группы. Добавлена функция заполнения полей группы

--- fixture/group.py
+++ fixture/group.py
@@ -11,18 +11,10 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        # init group creation
+        #init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        #fill group form
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_gropus_page()
@@ -47,7 +39,14 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         #click Edit group
         wd.find_element_by_name("edit").click()
-        # fill group form
+        #fill group form
+        self.fill_group_form(group)
+        #update group edition
+        wd.find_element_by_name("update").click()
+        self.return_to_gropus_page()
+
+    def fill_group_form(self,group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -57,6 +56,3 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        #update group edition
-        wd.find_element_by_name("update").click()
-        self.return_to_gropus_page()
