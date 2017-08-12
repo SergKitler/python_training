7016bb434464d7a61b8a6b8eecc43b61b8de819a
SergKitler <svkitler@gmail.com>
2017-08-12 22:02:31 +0700

    Добавлена проверка условия if-then-else в тест изменения группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,7 +3,12 @@ __author__ = 'sergei'
 from model.group import Group
 
 
-def test_edit_group(app):
+def test_edit_group_name(app):
      app.session.login(username="admin", password="secret")
-     app.group.edit_first_group(Group(name="TestEditGroup", header="TestEditGroupHeader", footer="TestEditGroupFooter"))
+     app.group.edit_first_group(Group(name="TestEditGroup2"))
+     app.session.logout()
+
+def test_edit_group_header(app):
+     app.session.login(username="admin", password="secret")
+     app.group.edit_first_group(Group(header="TestEditGroupHeader"))
      app.session.logout()
