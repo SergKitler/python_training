e0cc3b501b9ee3bb8696b198f9b709d68cabeee5
SergKitler <svkitler@gmail.com>
2017-08-16 21:56:32 +0700

    Добавлена проверка текушей страницы при переходах

--- fixture/user.py
+++ fixture/user.py
@@ -97,9 +97,11 @@ class UserHelper:
 
     def open_birthday_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("next birthdays").click()
+        if not wd.current_url.endswith("/birthdays.php"):
+            wd.find_element_by_link_text("next birthdays").click()
 
     def count(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/") and len(wd.find_elements_by_xpath("//table[@id='maintable']//a[.='Last name']")) > 0):
+            wd.find_element_by_link_text("home").click()
         return len(wd.find_elements_by_name("selected[]"))
