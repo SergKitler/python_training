91d7e25a74324ee82485d2ac5c032e5520e1b14f
SergKitler <svkitler@gmail.com>
2017-08-25 22:33:26 +0700

    Добавлена проверка списков групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,11 +4,17 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
+     old_groups = app.group.get_group_list()
      if app.group.count() == 0:
           app.group.create(Group(name="TestBeforeEditGroup"))
      app.group.edit_first_group(Group(name="TestEditGroup2"))
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
+     old_groups = app.group.get_group_list()
      if app.group.count() == 0:
           app.group.create(Group(header="TestBeforeEditGroupHeader"))
      app.group.edit_first_group(Group(header="TestEditGroupHeader"))
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) == len(new_groups)
