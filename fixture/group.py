47f3a418d15d43ed41ab56beaf001dc7bcae4cc0
SergKitler <svkitler@gmail.com>
2017-08-26 17:38:20 +0700

    Добавлена рандомизация в выбор группы для ее удаления и изменения

--- fixture/group.py
+++ fixture/group.py
@@ -27,18 +27,24 @@ class GroupHelper:
         wd.find_element_by_link_text("group page").click()
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_gropus_page()
         self.group_cache = None
 
     def edit_first_group(self,group):
+        self.edit_group_by_index(0)
+
+    def edit_group_by_index(self,index, group):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #click Edit group
         wd.find_element_by_name("edit").click()
         self.fill_group_form(group)
@@ -50,6 +56,10 @@ class GroupHelper:
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def fill_group_form(self,group):
         wd = self.app.wd
