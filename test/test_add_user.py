e70e4685d127e861c32cdf22b3f06a2bc6fc4d60
SergKitler <svkitler@gmail.com>
2017-09-09 11:51:54 +0700

    Добавлен генератор тестовых данных для теста добавления пользователя

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,43 +1,45 @@
 # -*- coding: utf-8 -*-
 
 from model.user import User
+import pytest
 import random
 import string
 
-Fname = ["Ivan", "Semen", "Serg", "Konstantin", "Nikolas", "Mosses"]
-Lname = ["Brown", "Kaddi", "Bethoven", "Radzinsky", "Runny", "Kazbek"]
-Address = ["Tomsk", "Los Angeles", "New-York", "Moscow", "Bangladesh"]
-domain = ["mail.ru", "gmail.com", "hotmail.ru", "my.ru"]
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_user(app):
+def random_phone(maxlen):
+    return  "+" +''.join([random.choice(string.digits) for i in range(maxlen)])
+
+def random_email(maxlen,maxdomainlen):
+    #symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    #domain = string.ascii_letters + string.digits + string.punctuation + " " * 10 + "." * 10
+    symbols = string.ascii_letters + string.digits + " " * 10
+    domain = string.ascii_letters + string.digits +  " " * 10 + "." * 10
+    return "@".join(["".join([random.choice(symbols) for i in range(random.randrange(maxlen))]),
+                     "".join([random.choice(domain) for i in range(random.randrange(maxdomainlen))])])
+
+testdata = [User(firstname="", lastname="", address="")] +[
+     User(firstname=random_string("Firstname",10),
+           lastname=random_string("Lastname",20),
+           address=random_string("Address",20),
+           home=random_phone(10),
+           mobile=random_phone(10),
+           work=random_phone(10),
+           phone2=random_phone(10),
+           email=random_email(12,12),
+           email2=random_email(12,12),
+           email3=random_email(12,12),
+           )
+     for i in range(2)
+]
+
+@pytest.mark.parametrize("user", testdata, ids=[repr(x) for x in testdata])
+def test_add_user(app, user):
     old_user = app.user.get_user_list()
-    user = User(firstname=random.choice(Fname),
-                lastname=random.choice(Lname),
-                address=random.choice(Address),
-                email="@".join([
-                    ''.join([random.choice(string.ascii_letters) for i in range(random.randrange(12))]),
-                        random.choice(domain)]),
-                email2="@".join([
-                    ''.join([random.choice(string.ascii_letters) for i in range(random.randrange(12))]),
-                    random.choice(domain)]),
-                email3="@".join([
-                    ''.join([random.choice(string.ascii_letters) for i in range(random.randrange(12))]),
-                    random.choice(domain)]),
-                home=''.join([random.choice(string.digits) for i in range(10)]),
-                mobile=''.join([random.choice(string.digits) for i in range(10)]),
-                work=''.join([random.choice(string.digits) for i in range(10)]),
-                phone2=''.join([random.choice(string.digits) for i in range(10)]))
     app.user.add(user)
     assert len(old_user) + 1 == app.user.count()
     new_user = app.user.get_user_list()
     old_user.append(user)
     assert sorted(old_user, key=User.id_or_max) == sorted(new_user, key=User.id_or_max)
-
-#def test_add_user2(app):
-#    old_user = app.user.get_user_list()
-#    user = User(firstname="Sanya", lastname="Beliy")
-#    app.user.add(user)
-#    assert len(old_user) + 1 == app.user.count()
-#    new_user = app.user.get_user_list()
-#    old_user.append(user)
-#    assert sorted(old_user, key=User.id_or_max) == sorted(new_user, key=User.id_or_max)
