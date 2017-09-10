406d26bbcf9d4d3efe49750cc00cebd24c3be857
SergKitler <svkitler@gmail.com>
2017-09-10 20:19:43 +0700

    Добавлена динамическая генерация тестов для добавления группы

--- generator/group.py
+++ generator/group.py
@@ -33,5 +33,3 @@ testdata = [Group(name="", header="", footer="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 with open(file, "w")as out:
     out.write(json.dumps(testdata, default=lambda x: x.__dict__,indent=2))
-
-
