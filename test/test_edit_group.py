6c2a5ca19d1d7203a035f16e2df4a2381a4eafb6
SergKitler <svkitler@gmail.com>
2017-09-17 12:05:17 +0700

    Изменены проверки в тестах для изменения групп через БД

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,23 +4,15 @@ from model.group import Group
 from random import randrange
 
 
-def test_edit_group_name(app):
-     if app.group.count() == 0:
+def test_edit_group_name(app, db):
+     if len(db.get_group_list()) == 0:
           app.group.create(Group(name="TestBeforeEditGroup"))
-     old_groups = app.group.get_group_list()
+     old_groups = db.get_group_list()
      index = randrange(len(old_groups))
-     group = Group(name="TestEditGroup2")
+     group = Group(name="TestEditGroup2", header="TestHeader2", footer="TestFooter2")
      group.id = old_groups[index].id
      app.group.edit_group_by_index(index, group)
      assert len(old_groups) == app.group.count()
-     new_groups = app.group.get_group_list()
+     new_groups = db.get_group_list()
      old_groups[index] = group
      assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-#def test_edit_group_header(app):
-#     old_groups = app.group.get_group_list()
-#     if app.group.count() == 0:
-#          app.group.create(Group(header="TestBeforeEditGroupHeader"))
-#     app.group.edit_first_group(Group(header="TestEditGroupHeader"))
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
