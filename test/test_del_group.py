47b01f04549f437afff96c34e940e3f1df08caf4
SergKitler <svkitler@gmail.com>
2017-09-17 11:52:43 +0700

    Добавлены отключаемые проверки в ui для тестов удаления групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,9 +3,7 @@ __author__ = 'sergei'
 from model.group import Group
 import random
 
-
-
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
      if len(db.get_group_list()) == 0:
           app.group.create(Group(name="TestBeforeDelGroup"))
      old_groups = db.get_group_list()
@@ -15,3 +13,6 @@ def test_delete_some_group(app, db):
      assert len(old_groups) - 1 == app.group.count()
      old_groups.remove(group)
      assert old_groups == new_groups
+     if check_ui:
+          assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
