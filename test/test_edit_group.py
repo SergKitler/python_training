79fc6f8772d3271acd6efe612f6a0ae6bc4cf2d0
SergKitler <svkitler@gmail.com>
2017-09-18 20:15:08 +0700

    Изменены проверки в тестах для добавления и изменения групп через БД и добавлены отключаемые проверки в ui

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,7 +4,7 @@ from model.group import Group
 from random import randrange
 
 
-def test_edit_group_name(app, db):
+def test_edit_group_name(app, db, check_ui):
      if len(db.get_group_list()) == 0:
           app.group.create(Group(name="TestBeforeEditGroup"))
      old_groups = db.get_group_list()
@@ -15,4 +15,8 @@ def test_edit_group_name(app, db):
      assert len(old_groups) == app.group.count()
      new_groups = db.get_group_list()
      old_groups[index] = group
-     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+     assert old_groups == new_groups
+     if check_ui:
+          def clean(group):
+               return Group(id=group.id, name=group.name.strip())
+          assert sorted(map(clean, new_groups), key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
