3caf2c952558d631c2a2ce70ec137ea240092120
SergKitler <svkitler@gmail.com>
2017-09-23 15:58:40 +0700

    Изменен тест для проверки информации о контактах на главной странице через БД

--- model/user.py
+++ model/user.py
@@ -36,7 +36,7 @@ class User:
                                          self.email, self.email2, self.email3)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname and self.address == other.address
 
     def id_or_max(self):
         if self.id:
--- test/test_contact.py
+++ test/test_contact.py
@@ -1,18 +1,20 @@
 __author__ = 'sergei'
 
 import re
-from random import randrange
+from model.user import User
 
 
-def test_contact_info_on_home_page(app):
-    index = randrange(app.user.count())
-    contact_from_home_page = app.user.get_user_list()[index]
-    contact_from_edit_page = app.user.get_user_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_email_from_page == merge_email_like_on_home_page(contact_from_edit_page)
+def test_contact_info_on_home_page(app, db):
+    contact_from_home_page = sorted(app.user.get_user_list(),  key=User.id_or_max)
+    contact_from_db = sorted(db.get_user_list(),  key=User.id_or_max)
+    phones_from_db = list(merge_phones_like_on_home_page(contact_from_db[i]) for i in range(len(contact_from_db)))
+    phones_from_home_page = list(contact_from_home_page[i].all_phones_from_page for i in range(len(contact_from_home_page)))
+    emails_from_db = list(merge_email_like_on_home_page(contact_from_db[i]) for i in range(len(contact_from_db)))
+    emails_from_home_page = list(contact_from_home_page[i].all_email_from_page for i in range(len(contact_from_home_page)))
+
+    assert contact_from_home_page == contact_from_db
+    assert phones_from_db == phones_from_home_page
+    assert emails_from_db == emails_from_home_page
 
 def clear(s):
     return re.sub("[() -]", "", s)
